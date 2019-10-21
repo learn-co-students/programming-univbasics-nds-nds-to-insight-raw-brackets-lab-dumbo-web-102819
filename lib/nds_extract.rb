7e4e741c66010d88a8a3cad1b257b4e56862811f
George Skrelja <gskrelja@gmail.com>
2019-10-21 00:40:30 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
+require 'pry'
 
 def directors_totals(nds)
   # Remember, it's always OK to pretty print what you get *in* to make sure
@@ -7,6 +8,7 @@ def directors_totals(nds)
   #
   #
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
+  pp nds
   result = {
   }
   #
@@ -18,5 +20,13 @@ def directors_totals(nds)
   #
   #
   # Be sure to return the result at the end!
-  nil
+
+  nds.each do |element|
+    gross = 0
+    element[:movies].each do |movie_hash|
+      gross += movie_hash[:worldwide_gross]
+      result[element[:name]] = gross
+    end
+  end
+  result
 end
